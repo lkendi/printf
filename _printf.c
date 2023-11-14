de803389c16d5f77512a0d3238626d624bb25faa
lkendi <lkendi003@gmail.com>
2023-11-14 12:30:33 +0300

    Added code to _printf.c

--- _printf.c
+++ _printf.c
@@ -3,12 +3,13 @@
 /**
  * _printf - produces output according to a format
  * @format: the format specifiers
- * Return: number of printed characters excluding \0
+ * Return: number of printed characters excluding '\0'
  */
 
 int _printf(const char *format, ...)
 {
 	int char_count = 0;
+	char c, *string;
 	va_list args_list;
 
 	if (format == NULL)
@@ -23,11 +24,16 @@ int _printf(const char *format, ...)
 			format++;
 			switch (*format)
 			{
+				case '\0':
+					break;
 				case '%':
-					/*code*/
+					write(1, format, 1);
+					char_count++;
 					break;
 				case 'c':
-					/*code*/
+					c = va_arg(args_list, int);
+					write(1, &c, 1);
+					char_count++;
 					break;
 				case 's':
 					/*code*/
