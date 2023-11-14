45d33c13ca43931f9e0b9b4b62f5fe7fdbc6b4b1
lkendi <lkendi003@gmail.com>
2023-11-14 15:58:44 +0300

    Added function implementations

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * print_char - prints integers for specifier c
  * @char_count:num of printed chars
@@ -9,32 +8,70 @@
 
 void print_char(int *char_count, va_list args_list)
 {
-    /*code for char*/
-}
+    char c;
 
+    c = va_arg(args_list, int);
+    write(1, &c, 1);
+    (*char_count)++;
+}
 
 /**
  * print_str - prints integers for specifier s
  * @char_count:num of printed chars
  * @args_list:list of args
 */
+
 void print_str(int *char_count, va_list args_list)
 {
-    /*code*/
-}
+	char *s;
 
+	s = va_arg(args_list, char*);
+	write(1, s, strlen(s));
+	(*char_count)++;
+}
 
 /**
  * print_integer - prints integers for specifier i and d
  * @char_count:num of printed chars
  * @args_list:list opf args
 */
+
 void print_integer(int *char_count, va_list args_list)
 {
-    /*code*/
+	int n, digits, i;
+	int temp;
+	char buffer[12];
+	
+	n = va_arg(args_list, int);
+	
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n *= -1;
+		(*char_count)++;
+	}
+	temp = n;
+	while (temp != 0)
+	{
+		temp /= 10;
+		digits++;
+	}
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		(*char_count)++;
+	}
+	else
+	{
+		for (i = 0; n != 0; i++)
+		{
+			buffer[i] = n % 10 + '0';
+			n /= 10;
+		}
+		write(1, buffer, i);
+		(*char_count) += i;
+	}
 }
-
-
 
 
 /**
@@ -42,16 +79,10 @@ void print_integer(int *char_count, va_list args_list)
 * @format: the format specifiers
 * Return: number of printed characters excluding '\0'
 */
+
 int _printf(const char *format, ...)
 {
 	int char_count = 0;
-
-	char c;
-
-	char *s;
-
-	int d;
-
 	va_list args_list;
 
 	if (format == NULL)
@@ -66,27 +97,21 @@ int _printf(const char *format, ...)
 			format++;
 			switch (*format)
 			{
-				case '\0':
-					break;
 				case '%':
 					write(1, format, 1);
 					char_count++;
 					break;
 				case 'c':
-					c = va_arg(args_list, int);
-					write(1, &c, 1);
-					char_count++;
+					print_char(&char_count, args_list);
 					break;
 				case 's':
-					s = va_arg(args_list, char*);
-					write(1, s, strlen(s));
-					char_count += strlen(s);
+					print_str(&char_count, args_list);
 					break;
 				case 'd':
 					print_integer(&char_count, args_list);
 					break;
 				case 'i':
-				    print_integer(&char_count, args_list);
+					print_integer(&char_count, args_list);
 				    break;
 			}
 		}
