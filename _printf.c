a78387b81d7ffe14742c98d3c084aa32a9a53e94
lkendi <139500082+lkendi@users.noreply.github.com>
2023-11-14 16:29:00 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,55 +1,4 @@
 #include "main.h"
-
-/**
-* _printf - produces output according to a format
-* @format: the format specifiers
-* Return: number of printed characters excluding '\0'
-*/
-
-int _printf(const char *format, ...)
-{
-	int char_count = 0;
-	va_list args_list;
-
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	va_start(args_list, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				char_count++;
-			}
-			if (*format == 'c')
-			{
-				print_char(&char_count, args_list);
-			}
-			if (*format == 's')
-			{
-				print_str(&char_count, args_list);
-			}
-			if (*format == 'd' || *format == 'i')
-			{
-				print_integer(&char_count, args_list);
-			}
-		}
-		else
-		{
-			write(1, format, 1);
-			char_count++;
-		}
-		format++;
-	}
-	va_end(args_list);
-	return (char_count);
-}
-
 /**
  * print_char - prints integers for specifier c
  * @char_count:num of printed chars
@@ -120,5 +69,56 @@ void print_integer(int *char_count, va_list args_list)
 		write(1, buffer, i);
 		(*char_count) += i;
 	}
+}
+
+
+/**
+* _printf - produces output according to a format
+* @format: the format specifiers
+* Return: number of printed characters excluding '\0'
+*/
+
+int _printf(const char *format, ...)
+{
+	int char_count = 0;
+	va_list args_list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(args_list, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				char_count++;
+			}
+			if (*format == 'c')
+			{
+				print_char(&char_count, args_list);
+			}
+			if (*format == 's')
+			{
+				print_str(&char_count, args_list);
+			}
+			if (*format == 'd' || *format == 'i')
+			{
+				print_integer(&char_count, args_list);
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			char_count++;
+		}
+		format++;
+	}
+	va_end(args_list);
+	return (char_count);
 }
 
