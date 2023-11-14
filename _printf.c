131ae524c87a05cf089796892d22b2b251a55c2c
lkendi <lkendi003@gmail.com>
2023-11-14 12:05:07 +0300

    Added code to _printf.c

--- _printf.c
+++ _printf.c
@@ -1,10 +1,43 @@
 #include "main.h"
+#include <stdarg.h>
 /**
- * _printf - produces output according to a format.
+ * _printf - produces output according to a format
  * @format: the format specifiers
- * Return:  the number of characters printed
-*/
+ * Return: number of printed characters excluding \0
+ */
+
 int _printf(const char *format, ...)
 {
+	int char_count = 0;
+	va_list args_list;
 
+	if (format == NULL)
+	{
+		exit(1);
+	}
+
+	va_start(args_list, format);
+	switch (*format)
+	{
+		case '%':
+			format++;
+			switch (*format)
+			{
+				case '%':
+					/*code*/
+					break;
+				case 'c':
+					/*code*/
+					break;
+				case 's':
+					/*code*/
+					break;
+			}
+		break;
+	default:
+		write(1, format, 1);
+		char_count++;
+		break;
+	}
 }
+
