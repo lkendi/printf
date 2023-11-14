bc11ed647f2a189d94ff3e8a4d6e2cac0593f6a6
Ridan Hawi <ridanhawy@gmail.com>
2023-11-14 11:50:14 +0300

    Created _printf.c

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - produces output according to a format.
+ * @format: the format specifiers
+ * Return:  the number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+
+}
